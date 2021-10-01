315e2f0ee59a9d7e418fbd85b969b84b95cb6b51
Jai Ranganathan <jai.ranganathan@gmail.com>
2021-09-30 18:28:07 -0700

    Update infographiq.js

--- libs/infographiq_latest/infographiq.js
+++ libs/infographiq_latest/infographiq.js
@@ -42,8 +42,10 @@ function link_svg({svg, csv, svg_id = 'svg', toc_id = 'toc', hover_color = 'yell
   // open up the svg in d3
   d3.xml(svg).then((f) => {
 
-console.error("Document: " + document.fullscreenEnabled);
-console.error("Webkit: " + document.webkitFullscreenEnabled);
+    var fullscreen = false;
+    if ( document.fullscreenEnabled || document.webkitFullscreenEnabled){ fullscreen = true;}
+
+console.error("Fullscreen: " + fullscreen);
 
     // Add button for full screen option
     d3.select("#" + toc_id).append("BUTTON")
