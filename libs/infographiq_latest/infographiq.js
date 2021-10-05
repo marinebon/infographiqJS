b64dc752cc1f626edc5c25c48f79ea900e8aa7d9
superjai <jai.ranganathan@gmail.com>
2021-10-05 00:37:14 -0700

    Update infographiq.js

--- libs/infographiq_latest/infographiq.js
+++ libs/infographiq_latest/infographiq.js
@@ -47,7 +47,7 @@ function link_svg({svg, csv, svg_id = 'svg', toc_id = 'toc', hover_color = 'yell
 
       // Add button for full screen option
       d3.select("#" + toc_id).append("BUTTON")
-        .text("Make image full screen 3")
+        .text("Make image full screen 4")
         .attr("style", "margin-bottom: 5px")
         .attr("class", "btn btn-info")
         .on("click", openFullScreen)
@@ -62,8 +62,8 @@ function link_svg({svg, csv, svg_id = 'svg', toc_id = 'toc', hover_color = 'yell
                     elem.webkitRequestFullscreen();
                     console.log('width: ' + elem.clientWidth);
                     console.log('height: ' + elem.clientHeight);
-           elem.style.width = "100%"; 
-           elem.style.height = "100%"; 
+           elem.style.width = "elem.clientWidth"; 
+           elem.style.height = "elem.clientHeight"; 
 
 
         }
