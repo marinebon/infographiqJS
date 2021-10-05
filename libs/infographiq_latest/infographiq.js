112bf5cc3498d8647e5096e68b77f86c7023037a
superjai <jai.ranganathan@gmail.com>
2021-10-05 00:22:57 -0700

    Update infographiq.js

--- libs/infographiq_latest/infographiq.js
+++ libs/infographiq_latest/infographiq.js
@@ -59,7 +59,10 @@ function link_svg({svg, csv, svg_id = 'svg', toc_id = 'toc', hover_color = 'yell
         if (elem.requestFullscreen) {
             elem.requestFullscreen();
         } else if (elem.webkitRequestFullscreen) { /* Safari */
-          elem.webkitRequestFullscreen();
+                    elem.webkitRequestFullscreen();
+           elem.style.width = "100%"; 
+           elem.style.height = "100%"; 
+
         }
       }
     }
